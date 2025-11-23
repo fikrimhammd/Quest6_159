7cc2b9375898b0917e4fe4f7f70aee6ffee9621c
fikrimhammd <fikri.kdi60@gmail.com>
2025-11-23 23:06:40 +0700

    Menyelesaikan seluruh codingan

--- app/src/main/java/com/example/myarsitekturmvvm/MainActivity.kt
+++ app/src/main/java/com/example/myarsitekturmvvm/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myarsitekturmvvm.ui.theme.MyArsitekturMVVMTheme
+import com.example.myarsitekturmvvm.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,16 +20,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyArsitekturMVVMTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                SiswaApp()
+
+
                 }
             }
         }
     }
-}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
